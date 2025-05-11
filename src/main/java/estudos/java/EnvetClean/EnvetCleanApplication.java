72b22a7be14fdc8bb303770c2377b93beaff030e
MarcosVinicius <marcusvinycius73@gmail.com>
2025-05-11 10:43:55 -0300

    initial commit

---
+++ src/main/java/estudos/java/EnvetClean/EnvetCleanApplication.java
@@ -0,0 +1,13 @@
+package estudos.java.EnvetClean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnvetCleanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnvetCleanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/estudos/java/EnvetClean/EnvetCleanApplicationTests.java
@@ -0,0 +1,13 @@
+package estudos.java.EnvetClean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnvetCleanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
